063b6789b50d6d3e6093f407e8b6db3ff56d91dc
NATALIAFUENTES <nataliaf1412@gmail.com>
2023-09-29 07:37:02 -0500

    "feat: servlets delete and update product"

--- src/main/java/com/purity/ecommerce/controllers/ProductController.java
+++ src/main/java/com/purity/ecommerce/controllers/ProductController.java
@@ -4,13 +4,13 @@ import com.purity.ecommerce.models.Product;
 import com.purity.ecommerce.repositories.ProductRepository;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import static java.util.stream.Collectors.toList;
 
@@ -18,8 +18,6 @@ import static java.util.stream.Collectors.toList;
 @RequestMapping("/api")
 public class ProductController {
 
-    @Value("${file.upload-dir}")
-    private String uploadDir;
 
     @Autowired
     private ProductRepository productRepository;
@@ -31,6 +29,14 @@ public class ProductController {
                 .map(ProductDTO::new)
                 .collect(toList());
 
+    }
+
+    @GetMapping("/product/{id}")
+    public ProductDTO getProduct(@PathVariable Long id) {
+        return productRepository
+                .findById(id)
+                .map(ProductDTO::new)
+                .orElse(null);
     }
 
     @PostMapping("/products/create")
@@ -105,4 +111,62 @@ public class ProductController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error uploading image to GitHub.");
         }
     }
+
+    @PatchMapping("/products/update/{productId}")
+    public ResponseEntity<String> updateProduct(
+            @PathVariable Long productId,
+            @RequestBody ProductDTO productDto) {
+
+        Optional<Product> optionalProduct = productRepository.findById(productId);
+
+        if (optionalProduct.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Product existingProduct = optionalProduct.get();
+
+        if (productDto.getName() != null) {
+            existingProduct.setName(productDto.getName());
+        }
+
+        if (productDto.getDescriptLong() != null) {
+            existingProduct.setDescriptLong(productDto.getDescriptLong());
+        }
+
+        if (productDto.getDescriptShort() != null) {
+            existingProduct.setDescriptShort(productDto.getDescriptShort());
+        }
+
+        if (productDto.getPrice() != null) {
+            existingProduct.setPrice(productDto.getPrice());
+        }
+
+        if (productDto.getCategory() != null) {
+            existingProduct.setCategory(productDto.getCategory());
+        }
+
+        if (productDto.getBrand() != null) {
+            existingProduct.setBrand(productDto.getBrand());
+        }
+
+        if (productDto.getStock() != null) {
+            existingProduct.setStock(productDto.getStock());
+        }
+
+        productRepository.save(existingProduct);
+
+        return ResponseEntity.ok("Product updated successfully.");
+    }
+
+    @DeleteMapping("/products/delete/{productId}")
+    public ResponseEntity<String> deleteProduct(@PathVariable Long productId) {
+        if (productRepository.existsById(productId)) {
+            productRepository.deleteById(productId);
+            return ResponseEntity.ok("Product deleted successfully.");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
 }
--- src/main/java/com/purity/ecommerce/dtos/CartItemDTO.java
+++ src/main/java/com/purity/ecommerce/dtos/CartItemDTO.java
@@ -11,7 +11,42 @@ public class CartItemDTO {
     public CartItemDTO(CartItem cartItem) {
         this.id = cartItem.getId();
         this.quantity = cartItem.getCount();
-        //this.cartId = cartItem.getCart().getId();
+        this.cartId = cartItem.getCart().getId();
         this.productDTO = new ProductDTO(cartItem.getProduct());
+    }
+
+    public CartItemDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Long getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(Long cartId) {
+        this.cartId = cartId;
+    }
+
+    public ProductDTO getProductDTO() {
+        return productDTO;
+    }
+
+    public void setProductDTO(ProductDTO productDTO) {
+        this.productDTO = productDTO;
     }
 }
