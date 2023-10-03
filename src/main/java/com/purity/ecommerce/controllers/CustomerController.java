e8cd6cbd84578eb3bb2b429a50d899ffb25c9389
NATALIAFUENTES <nataliaf1412@gmail.com>
2023-10-03 16:57:35 -0500

    "fix: payment controller n email confirmation"

--- src/main/java/com/purity/ecommerce/controllers/CustomerController.java
+++ src/main/java/com/purity/ecommerce/controllers/CustomerController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -22,6 +23,9 @@ public class CustomerController {
     private CustomerRepository customerRepository;
     @Autowired
     private CartRepository cartRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @GetMapping("/customers")
     public List<CustomerDTO> getCustomers() {
@@ -54,7 +58,7 @@ public class CustomerController {
                 customer.getName(),
                 customer.getEmail(),
                 customer.getAddress(),
-                customer.getPassword());
+                passwordEncoder.encode(customer.getPassword()));
         customerRepository.save(newCustomer);
 
         String sesionToken = (String) httpServletRequest.getSession(true).getAttribute("sesionToken");
--- src/main/java/com/purity/ecommerce/controllers/PaymentController.java
+++ src/main/java/com/purity/ecommerce/controllers/PaymentController.java
@@ -1,11 +1,17 @@
 package com.purity.ecommerce.controllers;
 import com.purity.ecommerce.dtos.PaymentRequestDTO;
+import com.purity.ecommerce.models.Customer;
+import com.purity.ecommerce.repositories.CustomerRepository;
+import com.purity.ecommerce.services.EmailService;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
 import com.stripe.param.PaymentIntentCreateParams;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -13,11 +19,21 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class PaymentController {
 
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private EmailService emailService;
     @Value("${stripe.secret.key}")
     private String stripeSecretKey; // Your Stripe public key
 
     @PostMapping("/process-payment")
-    public ResponseEntity<String> processPayment(@RequestBody PaymentRequestDTO paymentRequest) {
+    public ResponseEntity<String> processPayment(@RequestBody PaymentRequestDTO paymentRequest, Authentication authentication) {
+
+        if (authentication ==  null) {
+            return new ResponseEntity<>("You must login before checkout", HttpStatus.FORBIDDEN);
+
+        }
         try {
             Stripe.apiKey = stripeSecretKey;
 
@@ -32,6 +48,7 @@ public class PaymentController {
             if ("requires_action".equals(paymentIntent.getStatus())) {
 
             }
+            emailService.sendOrderConfirmationEmail(authentication.getName());
             return ResponseEntity.ok("Payment processed successfully.");
         } catch (StripeException e) {
             e.printStackTrace();
