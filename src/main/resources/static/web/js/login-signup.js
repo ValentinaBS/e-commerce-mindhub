634f6547fa29b56543dcd60a6a49b22db38f07de
ValentinaBS <vbsanchez16@gmail.com>
2023-10-03 16:03:39 -0300

    "refactor: admin page layout and search"

--- src/main/resources/static/web/js/login-signup.js
+++ src/main/resources/static/web/js/login-signup.js
@@ -35,7 +35,7 @@ const options = {
       axios.post("/api/login", `email=${this.emailInput}&password=${this.passwordInput}`)
           .then(res => {
               if (this.emailInput.startsWith("admin")) {
-                  window.location.href = "/web/pages/admin/product-manager.html"
+                  window.location.href = "/web/pages/admin/product-manager.html?category=lips"
               } else {
                   window.location.href = "/web/pages/products.html?category=all"
               }
